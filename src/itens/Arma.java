7086a1b8cfba84401487ce08fec3c76799d0883a
Diego Nery Romeiro <diego.nery@upe.br>
2025-04-06 16:42:11 -0300

    Criação da subclasse Arma com atributos específicos, encapsulamento e método de ataque
    Implementei a subclasse Arma estendendo a classe Item, com o objetivo de representar itens ofensivos usados em combate no jogo.
    
    A classe possui atributos próprios como tipo (ex: corpo a corpo, à distância), dano e distância de alcance, todos encapsulados com getters e setters, mantendo a consistência dos dados.
    
    Além disso, desenvolvi o método atacar(), que aplica dano a uma criatura utilizando a arma, reduzindo sua durabilidade a cada uso. Esse método já simula interações futuras com criaturas hostis do jogo e prepara o sistema para o combate.

---
+++ src/itens/Arma.java
@@ -0,0 +1,57 @@
+package itens; // Define que esta classe pertence ao pacote 'itens'
+
+//Imports necessarios para ter relação direta com criaturas
+
+// A classe Arma é uma subclasse da classe Item
+public class Arma extends Item{
+    private String tipo;
+    private double dano;
+    private double distancia;
+
+    //Construtor
+    public Arma(String nome, int peso, int durabilidade, String tipo, double dano, double distancia) {
+        super(nome, peso, durabilidade);
+        this.tipo = tipo;
+        this.dano = dano;
+        this.distancia = distancia;
+    }
+
+    //Metodo Getters
+    public String getTipo(){
+        return tipo;
+    }
+
+    public double getDano(){
+        return dano;
+    }
+
+    public double getDistancia(){
+        return distancia;
+    }
+
+    //Metodos Setters
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+
+    public void setDano(double dano){
+        this.dano = dano;
+    }
+
+    public void setDistancia(double distancia){
+        this.distancia = distancia;
+    }
+
+    //Metodo atacar
+    public void atacar(EventoCriatura inimigo) {
+        if (getDurabilidade() > 0) {
+            inimigo.receberDano(dano); // Aplica dano ao inimigo (precisa implementar em Alvo ou Inimigo)
+            System.out.println("Você atacou com uma arma " + tipo + " causando " + dano + " de dano.");
+            setDurabilidade(getDurabilidade() - 1); // Reduz durabilidade da arma
+        } else {
+            System.out.println("A arma está quebrada e não pode ser usada.");
+        }
+    }
+
+}
+
