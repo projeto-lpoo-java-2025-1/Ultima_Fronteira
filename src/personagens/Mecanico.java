609fd6d6c50ab0106018236382e9aa3f8879810f
Diego Nery Romeiro <diego.nery@upe.br>
2025-04-06 16:31:54 -0300

    Criação da subclasse Mecanico com herança da classe Personagem
    Implementei a subclasse Mecanico estendendo a superclasse Personagem, herdando todos os seus atributos e métodos.
    
    Neste momento, a classe possui apenas o construtor chamando o super, mas a estrutura já está pronta para receber futuramente métodos específicos que representem as habilidades únicas do personagem Mecanico.
    
    Essa separação por subclasses permite organizar melhor as funções de cada tipo de personagem no jogo, facilitando a manutenção e evolução do código.

---
+++ src/personagens/Mecanico.java
@@ -0,0 +1,15 @@
+package personagens; // Define que esta classe pertence ao pacote 'itens'
+
+
+import java.util.ArrayList; // Importa a classe ArrayList, usada como tipo do inventário do personagem
+
+// A classe Mecanico é uma subclasse da classe Personagem
+public class Mecanico extends Personagem {
+
+    //Construtor
+    public Mecanico(String nome, int vida, int fome, int sede, int energia, int sanidade, ArrayList inventario, String localizacao) {
+        super(nome, vida, fome, sede, energia, sanidade, inventario, localizacao);
+    }
+
+    //pretendo criar metodos com as skills dos itens por aqui
+}
