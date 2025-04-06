d9cd7debd74a57fbcde7a0fbc06f27f463af4ba7
Diego Nery Romeiro <diego.nery@upe.br>
2025-04-06 16:29:40 -0300

    Criação da superclasse Personagem com atributos, encapsulamento e métodos
    Implementei a superclasse Personagem que será utilizada como base para as demais classes de personagens do jogo.
    
    Adicionei os atributos principais (vida, fome, sede, energia, sanidade, etc.) e apliquei encapsulamento com métodos getters e setters para manter a segurança e controle dos dados.
    
    Também desenvolvi métodos de redução e recuperação de status (vida, fome, sede e energia), que poderão ser utilizados ou sobrescritos pelas subclasses específicas.
    
    Essa estrutura facilita a reutilização de código e a organização das lógicas comuns entre os personagens.

---
+++ src/personagens/Personagem.java
@@ -0,0 +1,132 @@
+package personagens; // Define que esta classe pertence ao pacote itens
+
+import personagens.Item; // Importa a classe Item do pacote personagens, necessária para manipular personagens no inventário
+import java.util.ArrayList; // Importa a classe ArrayList, usada para criar listas dinâmicas em Java
+
+public class Personagem { // Declaração da classe Personagem
+
+    // Atributos privados do personagem, acessíveis apenas pela própria classe
+    private String nome;
+    private int vida;
+    private int fome;
+    private int sede;
+    private int energia;
+    private int sanidade;
+    private ArrayList<Item> inventario;
+    private String localizacao;
+
+    // Construtor
+    public Personagem(String nome,int vida, int fome, int sede, int energia, int sanidade, ArrayList inventario, String localizacao){
+        this.nome = nome;
+        this.vida = vida;
+        this.fome = fome;
+        this.sede = sede;
+        this.energia = energia;
+        this.sanidade = sanidade;
+        this.inventario = new ArrayList<>();
+        this.localizacao = localizacao;
+
+
+    }
+
+    //metodos getters
+    public String getNome(){
+        return nome;
+    }
+
+    public int getVida(){
+        return vida;
+    }
+
+    public int getFome(){
+        return fome;
+    }
+
+    public int getSede(){
+        return sede;
+    }
+
+    public int getEnergia(){
+        return energia;
+    }
+
+    public int getSanidade(){
+        return sanidade;
+    }
+
+    public ArrayList<Item> getInventario(){
+        return inventario;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    // Métodos setters
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setVida(int vida){
+        this.vida = vida;
+    }
+
+    public void setFome(int fome){
+        this.fome = fome;
+    }
+
+    public void setSede(int sede){
+        this.sede = sede;
+    }
+
+    public void setEnergia(int energia){
+        this.energia = energia;
+    }
+
+    public void setSanidade(int sanidade){
+        this.sanidade = sanidade;
+    }
+
+    public void setInventario(ArrayList inventario) {
+        this.inventario = inventario;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    // métodos de vida, sede , fome e energia
+
+    public void reducaodeVida(int quantidade){
+        this.vida = Math.min(100, this.vida - quantidade);
+    }
+
+    public void reducaodeSede(int quantidade){
+        this.sede = Math.min(100, this.sede - quantidade);
+    }
+
+    public void reducaodeFome(int quantidade){
+        this.fome = Math.min(100, this.fome - quantidade);
+    }
+
+    public void reducaodeEnergia(int quantidade){
+        this.energia = Math.min(100, this.energia - quantidade);
+    }
+
+    public void recuperacaodeVida(int quantidade){
+        this.vida = Math.min(100, this.vida + quantidade);
+    }
+
+    public void recuperacaodeSede(int quantidade){
+        this.sede = Math.min(100, this.sede + quantidade);
+    }
+
+    public void recuperacaodeFome(int quantidade){
+        this.fome = Math.min(100, this.fome + quantidade);
+    }
+
+    public void recuperacaodeEnergia(int quantidade){
+        this.energia = Math.min(100, this.energia + quantidade);
+    }
+
+}
