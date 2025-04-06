114e84140d27c40b3e504894554ee4c3c193e7ed
Diego Nery Romeiro <diego.nery@upe.br>
2025-04-06 16:42:48 -0300

    Implementação da subclasse Ferramenta com atributos próprios, encapsulamento e método de uso
    Criei a subclasse Ferramenta como extensão da classe Item, com o objetivo de representar itens utilitários no jogo, como chaves inglesas, martelos ou outras ferramentas de manutenção.
    
    Implementei os atributos tipo (para identificar a função da ferramenta) e eficiência (representando sua capacidade de realizar ações), ambos encapsulados com métodos getters e setters.
    
    Adicionei o método usar(), que simula o uso da ferramenta e reduz sua durabilidade, preparando essa classe para interações futuras com outros objetos e sistemas do jogo, como reparos ou crafting.

---
+++ src/itens/Ferramenta.java
@@ -0,0 +1,42 @@
+package itens;// Define que esta classe pertence ao pacote 'itens'
+
+//Imports necessarios
+
+// A classe Ferramenta é uma subclasse da classe Item
+public class Ferramenta extends Item{
+    private String tipo;
+    private int eficiencia;
+
+    //Construtor
+    public Ferramenta(String nome, int peso, int durabilidade,String tipo, int eficiencia){
+        super(nome, peso, durabilidade);
+        this.tipo = tipo;
+        this.eficiencia = eficiencia;
+    }
+
+    //Método getters
+    public int getEficiencia(){
+        return eficiencia;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    //Métodos setters
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+
+    public void setEficiencia(int eficiencia){
+        this.eficiencia = eficiencia;
+    }
+
+    //Método usar
+    public void usar(){
+        setDurabilidade(getDurabilidade() - 1);
+        System.out.println("Você usou a ferramenta: " + getNome());
+    }
+
+
+}
