b2fec086db18fdc38c4f16c7dd16aab24da0dc99
Diego Nery Romeiro <diego.nery@upe.br>
2025-04-21 20:19:05 -0300

    Implementação do método 'usar' na classe Material

--- src/itens/Material.java
+++ src/itens/Material.java
@@ -1,43 +1,55 @@
 package itens; // Define que esta classe pertence ao pacote 'itens'
 
+import personagens.Personagem;
+
 // A classe Material é uma subclasse da classe Item
-public class Material extends Item{
+public abstract class Material extends Item {
     private String tipo;
     private int resistencia;
 
-    //Construtor
-    public Material(String nome, int peso, int durabilidade, String tipo, int resistencia){
+    // Construtor
+    public Material(String nome, int peso, int durabilidade, String tipo, int resistencia) {
         super(nome, peso, durabilidade);
         this.tipo = tipo;
         this.resistencia = resistencia;
     }
 
-    //Método getters
-    public String getTipo(){
+    // Métodos getters
+    public String getTipo() {
         return tipo;
     }
 
-    public int getResistencia(){
+    public int getResistencia() {
         return resistencia;
     }
 
-    //Método setters
-    public void setTipo(String tipo){
+    // Métodos setters
+    public void setTipo(String tipo) {
         this.tipo = tipo;
     }
 
-    public void setResistencia(int resistencia){
+    public void setResistencia(int resistencia) {
         this.resistencia = resistencia;
     }
 
-    //Metodo combinar
+    // Método combinar
     public Item combinar(Material outroMaterial) {
         String novoNome = this.getNome() + "-" + outroMaterial.getNome();
         int novoPeso = this.getPeso() + outroMaterial.getPeso();
         int novaDurabilidade = (this.getDurabilidade() + outroMaterial.getDurabilidade()) / 2;
         int eficienciaMedia = (this.resistencia + outroMaterial.resistencia) / 2;
+
+        // Criação da ferramenta combinada
         System.out.println("Você combinou " + this.getNome() + " com " + outroMaterial.getNome()
                 + " e criou: " + novoNome);
-        return new Ferramenta(novoNome, novoPeso, novaDurabilidade, "Ferramenta Combinada", eficienciaMedia);
+
+        // Agora, retornamos uma instância da ferramenta combinada
+        return new FerramentaCombinada(novoNome, novoPeso, novaDurabilidade, "Ferramenta Combinada", eficienciaMedia);
     }
+
+    public void usar(Personagem personagem, Material outroMaterial) {
+        combinar(outroMaterial);
+    }
+
 }
+
