6a25172b6a5fe5b27acfb52e4926f69efc39b930
leticiarodq <leticia.quirino@upe.br>
2025-04-22 22:36:30 -0300

    Polimorfismo na subclasse de Evento Climático
    Ajusta a subclasse EventoClimatico, corrigindo o construtor e simplificando a definição dos efeitos de cada evento. Foram adicionados métodos para acessar as propriedades de clima, duração e efeitos. O método executar() agora aplica impactos específicos para tempestade, nevasca e calor extremo, como redução de vida e aumento de sede, com mensagens detalhadas sobre os efeitos no personagem.

--- src/eventos/EventoClimatico.java
+++ src/eventos/EventoClimatico.java
@@ -2,6 +2,8 @@ package eventos;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import personagens.*;
 
 public class EventoClimatico extends Evento {
 
@@ -15,20 +17,50 @@ public class EventoClimatico extends Evento {
 
         super(nomeEvento, descricaoEvento, probabilidadeOcorrencia, impacto, condicaoAtivacao);
 
-        this.tipoClima=tipoClima;
-        this.duracao=duracao;
-        this.efeitoAmbiente=Arrays.asList(efeito);
+        this.tipoClima = tipoClima;
+        this.duracao = duracao;
+        this.efeitoAmbiente = Arrays.asList(efeito);
     }
 
-    public String getTipoClima(){
+    public String getTipoClima() {
         return tipoClima;
     }
 
-    public int getDuracao(){
+    public int getDuracao() {
         return duracao;
     }
 
-    public List<String> getEfeitoAmbiente(){
+    public List<String> getEfeitoAmbiente() {
         return efeitoAmbiente;
+    }
+
+    @Override
+    public void executar(Personagem personagem) {
+        switch (tipoClima.toLowerCase()) {
+            case "tempestade":
+                System.out.println("Iniciando tempestade: " + getNomeEvento() + " - Efeitos: " + getEfeitoAmbiente());
+                // Adicionar efeitos específicos para tempestade
+                personagem.reducaodeVida(10);
+                System.out.println("O personagem está com a roupa encharcada.");
+                System.out.println("A temperatura corporal do personagem baixou.");
+                System.out.println("A vida do personagem foi impacta em 10!");
+
+                break;
+            case "nevasca":
+                System.out.println("Iniciando nevasca: " + getNomeEvento() + " - Efeitos: " + getEfeitoAmbiente());
+                // Adicionar efeitos específicos para nevasca
+                personagem.reducaodeVida(20);
+
+                break;
+            case "calor extremo":
+                System.out.println("Iniciando calor extremo: " + getNomeEvento() + " - Efeitos: " + getEfeitoAmbiente());
+                // Adicionar efeitos específicos para calor extremo
+                personagem.reducaodeVida(15);
+                personagem.setSede(30);
+                break;
+            default:
+                System.out.println("Evento climático desconhecido.");
+
+        }
     }
 }
