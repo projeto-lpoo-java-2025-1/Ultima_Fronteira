44fcef8d7371bf376c422295177db724f8e233db
leticiarodq <leticia.quirino@upe.br>
2025-05-20 23:02:44 -0300

    Classe principal atualizada para suportar a Interface Gráfica

--- src/sistema/Main.java
+++ src/sistema/Main.java
@@ -1,20 +1,29 @@
-package sistema; // Define que esta classe faz parte do pacote 'sistema'
+package sistema;
 
-import personagens.Inventario;
-import personagens.Personagem; // Importa a classe Personagem para poder declarar variáveis desse tipo
+import gui.system.PainelJogo;
 
-public class Main { // Classe principal onde o programa começa sua execução
+import javax.swing.*;
 
-    public static void main(String[] args) { // Método main: ponto de entrada da aplicação Java
 
-        Jogo jogo = new Jogo();
+public class Main { // Define a classe Main, que contém o método principal do programa
 
-        jogo.iniciarAmbientes();
-        jogo.ocorrerEventoClimatico();
-        jogo.ocorrerEventoCriatura();
-        jogo.ocorrerEventoDoencaFerimento();
-        jogo.ocorrerEventoDescoberta();
+    public static void main(String[] args) { // O método main é o ponto de entrada do programa, onde começa a execução do jogo.
+
+        // Criação da janela
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Faz com que o programa termine completamente quando a janela for fechada.
+        window.setResizable(false); // Impede que o usuário redimensione a janela.
+        window.setTitle("Última Fronteira"); // Define título da janela
+
+        // Criação do painel do jogo
+        PainelJogo painelJogo = new PainelJogo();
+        window.add(painelJogo); // Adiciona o painel ao JFrame
+
+        window.pack(); // Ajusta o tamanho da janela para o painel
+        window.setLocationRelativeTo(null); // Centraliza a janela na tela
+        window.setVisible(true); // Torna a janela visível
+
+        //painelJogo.setupJogo();
+        painelJogo.iniciarThreadJogo();
     }
 }
-
-
